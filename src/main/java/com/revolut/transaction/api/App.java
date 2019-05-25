f7336f05fbb076d0d7273102acec72c6436f8f51
elangovan <elangovan_shanmugam@42-q.com>
2019-05-25 20:11:51 +0530

    Initial project setup

---
+++ src/main/java/com/revolut/transaction/api/App.java
@@ -0,0 +1,13 @@
+package com.revolut.transaction.api;
+
+/**
+ * Transcation api
+ *
+ */
+public class App {
+	
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+    
+}
